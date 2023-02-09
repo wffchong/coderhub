5029dcee1bf29b2aa1ef948099b7ea9aa9e384b1
wffchong <283953455@qq.com>
2023-02-09 19:08:42 +0800

    feat: 将动态和用户关联

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -8,7 +8,13 @@ class MomentService {
   }
 
   async queryList(offset = 0, size = 10) {
-    const statement = `SELECT * FROM moment LIMIT ? OFFSET ?`
+    const statement = `
+      SELECT m.id AS id, m.content as content, m.createAt AS createTime, m.updateAt AS updateTime,
+      JSON_OBJECT('id', u.id, 'username', u.username, 'createTime', u.createAt, 'updateTime', u.updateAt) AS user
+      FROM moment AS m
+      LEFT JOIN user AS u ON u.id = m.user_id
+      LIMIT ? OFFSET ?
+    `
     const [result] = await connection.execute(statement, [String(size), String(offset)])
     return result
   }
