b655d44d6cfa0b1bcb04cf48b76bb9199df0ccf4
wffchong <283953455@qq.com>
2023-02-09 14:29:38 +0800

    feat: 添加一些常量的配置

---
+++ src/config/server.js
@@ -0,0 +1,9 @@
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const { SERVER_PORT } = process.env
+
+module.exports = {
+  SERVER_PORT
+}
--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 const app = require('./app')
+const { SERVER_PORT } = require('./config/server')
+
 require('./utils/handle-error')
 
-app.listen(8000, () => {
+app.listen(SERVER_PORT, () => {
   console.log('服务器启动成功')
 })
